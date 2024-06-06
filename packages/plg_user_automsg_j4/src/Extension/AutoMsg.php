7d868e57ce59f42897c48f1bd9e1292c9d77dea7
Pascal Leconte <pascal.leconte@conseilgouz.com>
2024-06-06 17:22:13 +0200

    4.2.1

--- packages/plg_user_automsg_j4/src/Extension/AutoMsg.php
+++ packages/plg_user_automsg_j4/src/Extension/AutoMsg.php
@@ -1,11 +1,13 @@
 <?php
 /**
-* AutoMsg Profile  
+* AutoMsg Profile
 * Package			: Joomla 4.x/5.x
 * copyright 		: Copyright (C) 2024 ConseilGouz. All rights reserved.
 * license    		: https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
 */
+
 namespace ConseilGouz\Plugin\User\AutoMsg\Extension;
+
 defined('JPATH_BASE') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Form\Form;
@@ -15,10 +17,11 @@ use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\Database\DatabaseAwareTrait;
 use Joomla\Event\SubscriberInterface;
 
-class AutoMsg extends CMSPlugin implements SubscriberInterface {
+class AutoMsg extends CMSPlugin implements SubscriberInterface
+{
     use DatabaseAwareTrait;
 
-protected $db;
+    protected $db;
 
     /**
      * @inheritDoc
@@ -37,34 +40,36 @@ protected $db;
         ];
     }
 
-	/**
-	 * @param	string	The context for the data
-	 * @param	int		The user id
-	 * @param	object
-	 * @return	boolean
-	 * @since	1.6
-	 */
-	public function prepareData($event)
-	{
+    /**
+     * @param	string	The context for the data
+     * @param	int		The user id
+     * @param	object
+     * @return	boolean
+     * @since	1.6
+     */
+    public function prepareData($event)
+    {
         $context    = $event[0];
         $data       = $event[1];
 
-		// Check we are manipulating a valid form.
-		if (!in_array($context, array('com_users.profile','com_users.registration','com_users.user','com_admin.profile','com_automsg.automsg'))){
-			return true;
-		}
-        
+        // Check we are manipulating a valid form.
+        if (!in_array($context, array('com_users.profile','com_users.registration','com_users.user','com_admin.profile','com_automsg.automsg'))) {
+            return true;
+        }
+
         $userId = isset($data->id) ? $data->id : 0;
-        if (!$userId) return false;
+        if (!$userId) {
+            return false;
+        }
         // Load the profile data from the database.
         $db    = $this->db;
         $query = $db->getQuery(true)
                  ->select(
-                        [
+                     [
                             $db->quoteName('profile_key'),
                             $db->quoteName('profile_value'),
                         ]
-                    )
+                 )
                 ->from($db->quoteName('#__user_profiles'))
                 ->where($db->quoteName('user_id') . ' = :userid')
                 ->where($db->quoteName('profile_key') . ' LIKE '.$db->quote('profile_automsg.automsg'))
@@ -73,94 +78,91 @@ protected $db;
 
         $db->setQuery($query);
         $results = $db->loadRowList();
- 
-		// Merge the profile data.
-		$data->profile_automsg =array();
-		foreach ($results as $v) {
-			$k = str_replace('profile_automsg.', '', $v[0]);
-			$data->profile_automsg[$k] = json_decode($v[1], true);
-		}
- 
-		return true;
-	}
- 
-	/**
-	 * @param	JForm	The form to be altered.
-	 * @param	array	The associated data for the form.
-	 * @return	boolean
-	 * @since	1.6
-	 */
-	public function prepareForm($event)
-	{
-		// Load user_profile plugin language
-		$lang = Factory::getLanguage();
-		$lang->load('plg_user_automsg', JPATH_ADMINISTRATOR);
+
+        // Merge the profile data.
+        $data->profile_automsg = array();
+        foreach ($results as $v) {
+            $k = str_replace('profile_automsg.', '', $v[0]);
+            $data->profile_automsg[$k] = json_decode($v[1], true);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param	JForm	The form to be altered.
+     * @param	array	The associated data for the form.
+     * @return	boolean
+     * @since	1.6
+     */
+    public function prepareForm($event)
+    {
+        // Load user_profile plugin language
+        $lang = Factory::getApplication()->getLanguage();
+        $lang->load('plg_user_automsg', JPATH_ADMINISTRATOR);
 
         $form       = $event[0];
         $data       = $event[1];
-    
-		if (!($form instanceof Form)) {
-			// $this->_subject->setError('JERROR_NOT_A_FORM');
-			return false;
-		}
-		// Check we are manipulating a valid form.
-		if (!in_array($form->getName(), array('com_users.profile', 'com_users.registration','com_users.user','com_admin.profile','com_automsg.automsg'))) {
-			return true;
-		}
-		if ($form->getName()=='com_users.profile')
-		{
-			// Add the profile fields to the form.
-		    $test = dirname(__FILE__);
-			Form::addFormPath($test.'/../profiles');
-			$form->loadFile('profile', false);
- 
-			// Toggle whether the something field is required.
-			if ($this->params->get('profile-require_something', 1) > 0) {
-				$form->setFieldAttribute('something', 'required', $this->params->get('profile-require_something') == 2, 'profile_automsg');
-			} else {
-				$form->removeField('something', 'profile_automsg');
-			}
-		}
- 
-		//In this example, we treat the frontend registration and the back end user create or edit as the same. 
-		elseif ($form->getName()=='com_users.registration' || $form->getName()=='com_users.user' || $form->getName()=='com_automsg.automsg' )
-		{		
-			// Add the registration fields to the form.
-		    $test = dirname(__FILE__);
-		    Form::addFormPath($test.'/../profiles');
-		    $form->loadFile('profile', false);
- 
-			// Toggle whether the something field is required.
-			if ($this->params->get('register-require_something', 1) > 0) {
-				$form->setFieldAttribute('something', 'required', $this->params->get('register-require_something') == 2, 'profile_automsg');
-			} else {
-				$form->removeField('something', 'profile_automsg');
-			}
-		}
-	}
- 
-	public function userAfterSave($event)
-	{
-        $data   = $event[0]; 
-        $data   = $event[1];
-        $isNew  = $event[2];
-        $result = $event[3];
-        $error  = $event[4];
-        
-		$userId	= (int)$data['id'];
- 
-		if ($userId && $result && isset($data['profile_automsg']) && (count($data['profile_automsg']))) {
-			$db = $this->db;
-			$db->setQuery(
-					$db->getQuery(true)
-							->delete($db->quoteName('#__user_profiles'))
-							->where($db->quoteName('user_id').' = :userId AND profile_key LIKE \'profile_automsg.automsg\'')
-							->bind(':userId', $userId, ParameterType::INTEGER)
-							)->execute();
-			$order	= 1;
+
+        if (!($form instanceof Form)) {
+            // $this->_subject->setError('JERROR_NOT_A_FORM');
+            return false;
+        }
+        // Check we are manipulating a valid form.
+        if (!in_array($form->getName(), array('com_users.profile', 'com_users.registration','com_users.user','com_admin.profile','com_automsg.automsg'))) {
+            return true;
+        }
+        if ($form->getName() == 'com_users.profile') {
+            // Add the profile fields to the form.
+            $test = dirname(__FILE__);
+            Form::addFormPath($test.'/../profiles');
+            $form->loadFile('profile', false);
+
+            // Toggle whether the something field is required.
+            if ($this->params->get('profile-require_something', 1) > 0) {
+                $form->setFieldAttribute('something', 'required', $this->params->get('profile-require_something') == 2, 'profile_automsg');
+            } else {
+                $form->removeField('something', 'profile_automsg');
+            }
+        }
+
+        //In this example, we treat the frontend registration and the back end user create or edit as the same.
+        elseif ($form->getName() == 'com_users.registration' || $form->getName() == 'com_users.user' || $form->getName() == 'com_automsg.automsg') {
+            // Add the registration fields to the form.
+            $test = dirname(__FILE__);
+            Form::addFormPath($test.'/../profiles');
+            $form->loadFile('profile', false);
+
+            // Toggle whether the something field is required.
+            if ($this->params->get('register-require_something', 1) > 0) {
+                $form->setFieldAttribute('something', 'required', $this->params->get('register-require_something') == 2, 'profile_automsg');
+            } else {
+                $form->removeField('something', 'profile_automsg');
+            }
+        }
+    }
+
+    public function userAfterSave($event)
+    {
+        $data   = $event[0];
+        $isNew  = $event[1];
+        $result = $event[2];
+        $error  = $event[3];
+
+        $userId	= (int)$data['id'];
+
+        if ($userId && $result && isset($data['profile_automsg']) && (count($data['profile_automsg']))) {
+            $db = $this->db;
+            $db->setQuery(
+                $db->getQuery(true)
+                            ->delete($db->quoteName('#__user_profiles'))
+                            ->where($db->quoteName('user_id').' = :userId AND profile_key LIKE \'profile_automsg.automsg\'')
+                            ->bind(':userId', $userId, ParameterType::INTEGER)
+            )->execute();
+            $order	= 1;
             $query = $db->getQuery(true)
                 ->insert($db->quoteName('#__user_profiles'));
-			foreach ($data['profile_automsg'] as $k => $v) {
+            foreach ($data['profile_automsg'] as $k => $v) {
                 $query->values(
                     implode(
                         ',',
@@ -180,25 +182,26 @@ protected $db;
                         )
                     )
                 );
-			}
+            }
             $db->setQuery($query);
             $db->execute();
-			$this->checkautomsgtoken($userId);
-		}
- 
-		return true;
-	}
-	/* check if automsg token exists.
-	*  if it does not, create it
-	*/
-    protected function checkautomsgtoken($userId) {
+            $this->checkautomsgtoken($userId);
+        }
+
+        return true;
+    }
+    /* check if automsg token exists.
+    *  if it does not, create it
+    */
+    protected function checkautomsgtoken($userId)
+    {
         $db    = $this->db;
         $query = $db->getQuery(true)
                  ->select(
-                        [
+                     [
                             $db->quoteName('profile_value'),
                         ]
-                    )
+                 )
                 ->from($db->quoteName('#__user_profiles'))
                 ->where($db->quoteName('user_id') . ' = :userid')
                 ->where($db->quoteName('profile_key') . ' LIKE '.$db->quote('profile_automsg.token'))
@@ -206,66 +209,68 @@ protected $db;
 
         $db->setQuery($query);
         $result = $db->loadResult();
-		if ($result) return true; // automsg token already exists => exit
-		// create a token
+        if ($result) {
+            return true;
+        } // automsg token already exists => exit
+        // create a token
         $query = $db->getQuery(true)
                 ->insert($db->quoteName('#__user_profiles'));
-		$token = mb_strtoupper(strval(bin2hex(openssl_random_pseudo_bytes(16))));
-		$order = 2;
-		$query->values(
-                implode(
-                        ',',
-                        $query->bindArray(
-                            [
-                                $userId,
-                                'profile_automsg.token',
-                                $token,
-                                $order++,
-                            ],
-                            [
-                                ParameterType::INTEGER,
-                                ParameterType::STRING,
-                                ParameterType::STRING,
-                                ParameterType::INTEGER,
-                            ]
-                        )
-                    )
+        $token = mb_strtoupper(strval(bin2hex(openssl_random_pseudo_bytes(16))));
+        $order = 2;
+        $query->values(
+            implode(
+                ',',
+                $query->bindArray(
+                    [
+                        $userId,
+                        'profile_automsg.token',
+                        $token,
+                        $order++,
+                    ],
+                    [
+                        ParameterType::INTEGER,
+                        ParameterType::STRING,
+                        ParameterType::STRING,
+                        ParameterType::INTEGER,
+                    ]
+                )
+            )
         );
         $db->setQuery($query);
         $db->execute();
-	}
-	/**
-	 * Remove all user profile information for the given user ID
-	 *
-	 * Method is called after user data is deleted from the database
-	 *
-	 * @param	array		$user		Holds the user data
-	 * @param	boolean		$success	True if user was succesfully stored in the database
-	 * @param	string		$msg		Message
-	 */
-	function userAfterDelete($event)
-	{
+    }
+    /**
+     * Remove all user profile information for the given user ID
+     *
+     * Method is called after user data is deleted from the database
+     *
+     * @param	array		$user		Holds the user data
+     * @param	boolean		$success	True if user was succesfully stored in the database
+     * @param	string		$msg		Message
+     */
+    public function userAfterDelete($event)
+    {
         $user       = $event[0];
         $success    = $event[1];
         $msg        = $event[2];
-        
-		if (!$success) {
-			return false;
-		}
- 
-		$userId	= $user['id'];
- 
-		if ($userId) {
-			$this->db->setQuery(
-    				$this->db->getQuery(true)
-							->delete($this->db->quoteName('#__user_profiles'))
-							->where($this->db->quoteName('user_id').' = :userId AND profile_key LIKE \'profile_automsg.%\'')
-							->bind(':userId', $userId, ParameterType::INTEGER)
-							)->execute();
-		}
- 
-		return true;
-	}
- 
- 
- }
+
+        if (!$success) {
+            return false;
+        }
+
+        $userId	= $user['id'];
+
+        if ($userId) {
+            $this->db->setQuery(
+                $this->db->getQuery(true)
+                            ->delete($this->db->quoteName('#__user_profiles'))
+                            ->where($this->db->quoteName('user_id').' = :userId AND profile_key LIKE \'profile_automsg.%\'')
+                            ->bind(':userId', $userId, ParameterType::INTEGER)
+            )->execute();
+        }
+
+        return true;
+    }
+
+
+}
